4729efdda308f5032b0aa536454b714a01904203
PC <wew2211wew@gmail.com>
2024-07-21 21:59:34 +0300

    add pic in sign up

--- src/main/java/com/example/databaseproject/HelloApplication.java
+++ src/main/java/com/example/databaseproject/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root=FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-        stage.setScene(new Scene(root));
+       stage.setScene(new Scene(root,850,500));
         stage.setTitle("Hello!");
         stage.setResizable(false);
         stage.initStyle(StageStyle.DECORATED);
