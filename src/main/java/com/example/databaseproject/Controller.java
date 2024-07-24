680e9da1344aa441e7fb91b774a40895e5f9279f
PC <wew2211wew@gmail.com>
2024-07-24 14:35:09 +0300

    edit buy car

---
+++ src/main/java/com/example/databaseproject/Controller.java
@@ -0,0 +1,33 @@
+package com.example.databaseproject;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+    @FXML
+    private Slider PriceSlider;
+    @FXML
+    private Label price;
+    int value;
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        value = (int) PriceSlider.getValue();
+        price.setText(Integer.toString(value) + "$");
+        PriceSlider.valueProperty().addListener(new ChangeListener<Number>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+                value = (int) PriceSlider.getValue();
+                price.setText(Integer.toString(value) + "$");
+            }
+        });
+    }
+}
--- src/main/java/com/example/databaseproject/LoginController.java
+++ src/main/java/com/example/databaseproject/LoginController.java
@@ -1,24 +1,30 @@
 package com.example.databaseproject;
 
 import javafx.animation.FadeTransition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.IOException;
-
-public class LoginController {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import  java.net.URL;
+public class LoginController
+{
     @FXML
     private TextField Username;
     @FXML
@@ -37,6 +43,7 @@ public class LoginController {
     private AnchorPane ap;
     @FXML
     private AnchorPane primaryAp;
+
 
 
     public void login(ActionEvent event) throws IOException {
@@ -61,18 +68,15 @@ public class LoginController {
             scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
+            } else {
 
-        } else {
-            sucess.setVisible(false);
-            wrong.setVisible(true);
-            wrong.setText("Incorrect username or password");
+                wrong.setVisible(true);
+                sucess.setVisible(false);
+                wrong.setText("Incorrect UserName or Password");
+            }
         }
-    }
-    @FXML
-    public void BuyCarPage(ActionEvent event)throws IOException
-    {
-   loadPage("BuyCar");
-    }
+
+
 
     @FXML
     private void Home(ActionEvent event) throws IOException {
@@ -109,15 +113,19 @@ public class LoginController {
     {
         Parent root = FXMLLoader.load(getClass().getResource(page +".fxml"));
         ap.getChildren().clear();
-        ap.getChildren().add(root);
+        //ap.getChildren().add(root);
+        ap.getChildren().addAll(root);
 
     }
-    public void loadPage1(String page) throws IOException {
+    public void loadPage1(String page) throws IOException
+    {
         Parent root = FXMLLoader.load(getClass().getResource(page +".fxml"));
-        primaryAp.getChildren().clear();
+       primaryAp.getChildren().clear();
         primaryAp.getChildren().add(root);
+
+
     }
 
 
 
-}
+};
