84db399d2746fbfc061fbd1a1311767e1eca3c00
PC <wew2211wew@gmail.com>
2024-07-26 17:22:27 +0300

    page1 buy car edit

--- src/main/java/com/example/databaseproject/CarPageController.java
+++ src/main/java/com/example/databaseproject/CarPageController.java
@@ -1,4 +1,5 @@
 package com.example.databaseproject;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -15,24 +16,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class CarPageController implements Initializable
-{
+public class CarPageController implements Initializable {
     @FXML
     private GridPane CarContainer;
+
     @FXML
-    private HBox CarLayout;
-    private List<Car>recentlyAdded;
+    private HBox NewCar;
+
+    private List<Car> recentlyAdded;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        recentlyAdded = recentlyAdded(); // Corrected method name
+
+
+        int row = 1;
         int coul=0;
-        int row=1;
-        recentlyAdded=new ArrayList<>(recentlyAdded());
         for(Car car : recentlyAdded)
         {
             try {
                 FXMLLoader fxmlLoader = new FXMLLoader();
                 fxmlLoader.setLocation(getClass().getResource("carlist.fxml"));
-               VBox cardbox = fxmlLoader.load();
+                VBox cardbox = fxmlLoader.load();
                 CardController cardController = fxmlLoader.getController();
                 cardController.setData(car);
                 if(coul==3)
@@ -40,91 +46,76 @@ public class CarPageController implements Initializable
                     coul=0;
                     row++;
                 }
-CarContainer.add(cardbox,coul++,row);
-                GridPane.setMargin(cardbox,new Insets(8));
+                CarContainer.add(cardbox,coul++,row);
+                GridPane.setMargin(cardbox, new Insets(8));
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
+
+      
     }
-    private List<Car>recentlyAdded()
-    {
-        List<Car> ls=new ArrayList<>();
-        Car car = new Car();
-        car.setYear("2019");
-        car.setType("sedan");
-        car.setTrans("auto");
-        car.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
-        car.setDiscribe("abood");
-        car.setMake("skoda");
 
+    private List<Car> recentlyAdded() {
+        List<Car> ls = new ArrayList<>();
 
-        car = new Car();
-        car.setYear("2019");
-        car.setType("sedan");
-        car.setTrans("auto");
-        car.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
-        car.setDiscribe("just used for 3 months");
-        car.setMake("skoda");
-        ls.add(car);
+        Car car1 = new Car();
+        car1.setYear("2019");
+        car1.setType("sedan");
+        car1.setTrans("auto");
+        car1.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
+        car1.setDiscribe("Just used for 3 months");
+        car1.setMake("skoda");
+        ls.add(car1);
 
-        car = new Car();
-        car.setYear("2019");
-        car.setType("sedan");
-        car.setTrans("auto");
-        car.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
-        car.setDiscribe("just used for 3 months");
-        car.setMake("skoda");
-        ls.add(car);
-        car = new Car();
-        car.setYear("2019");
-        car.setType("sedan");
-        car.setTrans("auto");
-        car.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
-        car.setDiscribe("just used for 3 months");
-        car.setMake("skoda");
-        ls.add(car);
-        car = new Car();
-        car.setYear("2019");
-        car.setType("sedan");
-        car.setTrans("auto");
-        car.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
-        car.setDiscribe("just used for 3 months");
-        car.setMake("skoda");
-        ls.add(car);
-        car = new Car();
-        car.setYear("2019");
-        car.setType("sedan");
-        car.setTrans("auto");
-        car.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
-        car.setDiscribe("just used for 3 months");
-        car.setMake("skoda");
-        ls.add(car);
-        car = new Car();
-        car.setYear("2019");
-        car.setType("sedan");
-        car.setTrans("auto");
-        car.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
-        car.setDiscribe("just used for 3 months");
-        car.setMake("skoda");
-        ls.add(car);
-        car = new Car();
-        car.setYear("2019");
-        car.setType("sedan");
-        car.setTrans("auto");
-        car.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
-        car.setDiscribe("just used for 3 months");
-        car.setMake("skoda");
-        ls.add(car);
+        car1 = new Car();
+        car1.setYear("2019");
+        car1.setType("sedan");
+        car1.setTrans("auto");
+        car1.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
+        car1.setDiscribe("Just used for 3 months");
+        car1.setMake("skoda");
+        ls.add(car1);
 
+        car1 = new Car();
+        car1.setYear("2019");
+        car1.setType("sedan");
+        car1.setTrans("auto");
+        car1.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
+        car1.setDiscribe("Just used for 3 months");
+        car1.setMake("skoda");
+        ls.add(car1);
 
-        ls.add(car);
+       car1 = new Car();
+        car1.setYear("2019");
+        car1.setType("sedan");
+        car1.setTrans("auto");
+        car1.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
+        car1.setDiscribe("Just used for 3 months");
+        car1.setMake("skoda");
+        ls.add(car1);
 
+         car1 = new Car();
+        car1.setYear("2019");
+        car1.setType("sedan");
+        car1.setTrans("auto");
+        car1.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
+        car1.setDiscribe("Just used for 3 months");
+        car1.setMake("skoda");
+        ls.add(car1);
+
+         car1 = new Car();
+        car1.setYear("2019");
+        car1.setType("sedan");
+        car1.setTrans("auto");
+        car1.setImagSrc("/main/resources/1200px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg");
+        car1.setDiscribe("Just used for 3 months");
+        car1.setMake("skoda");
+        ls.add(car1);
 
 
         return ls;
-
-
-
     }
+
+
 }
