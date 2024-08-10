1ab28a80a431bac04ba990f37f687e5135462394
PC <wew2211wew@gmail.com>
2024-08-10 04:45:16 +0300

    car detalis4

---
+++ src/main/java/com/example/databaseproject/Review.java
@@ -0,0 +1,24 @@
+package com.example.databaseproject;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+
+public class Review {
+
+    @FXML
+    private RadioButton op1;
+
+    @FXML
+    private RadioButton op2;
+
+    @FXML
+    private RadioButton op3;
+
+    @FXML
+    private RadioButton op4;
+
+    @FXML
+    private RadioButton op5;
+
+
+}
